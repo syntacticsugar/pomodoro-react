10dbe899d11e8bd71fb236bd473c0e671e20202b
Saerom Hong <hello@romcreative.com>
2016-06-14 12:33:05 -0400

    Adds <DistractionsList/>,but WTF is w my prev commit msg?

---
+++ scripts/components/DistractionsList.js
@@ -0,0 +1,15 @@
+'use strict'
+
+import React from 'react';
+
+var DistractionsList = React.createClass({
+  render : function() {
+    return (
+      <div>
+        Trivial sundries.
+      </div>
+    )
+  }
+});
+
+export default DistractionsList;
